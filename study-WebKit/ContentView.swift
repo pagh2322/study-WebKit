95d11faaa6da8f9b6c2135d71a92914df49b1a5c
pagh2322 <pagh2322@naver.com>
2022-09-06 00:09:21 +0900

    first commit

---
+++ study-WebKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  study-WebKit
+//
+//  Created by peo on 2022/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ study-WebKit/study_WebKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_WebKitApp.swift
+//  study-WebKit
+//
+//  Created by peo on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct study_WebKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
